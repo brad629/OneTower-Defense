528084f30056bd0598be0c460d2c0a9946bed639
bradley huntington <bradleyhuntington@bradley-huntingtons-macbook-pro.local>
2016-04-15 02:02:47 -0400

    Wall fix

--- OneTowerDefense/Wall.swift
+++ OneTowerDefense/Wall.swift
@@ -17,4 +17,8 @@ class Wall : SKSpriteNode{
         self.position = position
         
     }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 }
