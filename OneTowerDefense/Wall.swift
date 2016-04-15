c5fb2f55482823c54a6ce16226fac07fc938a244
bradley huntington <bradleyhuntington@bradleygtonsmbp.home>
2016-04-15 02:02:12 -0400

    Wall class

---
+++ OneTowerDefense/Wall.swift
@@ -0,0 +1,20 @@
+//
+//  Wall.swift
+//  OneTowerDefense
+//
+//  Created by bradley huntington on 4/15/16.
+//  Copyright © 2016 brad huntington. All rights reserved.
+//
+
+import SpriteKit
+
+class Wall : SKSpriteNode{
+    var hasTurret = false
+    
+    init (position: CGPoint){
+        let texture = SKTexture (imageNamed: "spr_rock")
+        super.init(texture: texture,color:SKColor(),size:texture.size())
+        self.position = position
+        
+    }
+}
