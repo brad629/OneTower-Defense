ccffee8bc86e98b8da26e1348d3d16d88c9873f0
bradley huntington <bradleyhuntington@bradleygtonsmbp.home>
2016-05-12 21:14:50 -0400

    updatebullet

--- OneTowerDefense/Enemies/Bullet.swift
+++ OneTowerDefense/Enemies/Bullet.swift
@@ -9,14 +9,19 @@
 import SpriteKit
 
 class Bullet : SKSpriteNode {
+    var target : Enemy
+    var timeToLive = 50
+    var damage : Double
+    var slow : Int
+    var snare : Bool
+    var areaOfEffect : Int
     
     init (position: CGPoint, damage: Double, slow: Int, snare: Bool, areaOfEffect: Int, target: Enemy) {
-        var position = position
-        var damage = damage
-        var slow = slow
-        var snare = snare
-        var areaOfEffect = areaOfEffect
-        var target = target
+        self.damage = damage
+        self.slow = slow
+        self.snare = snare
+        self.areaOfEffect = areaOfEffect
+        self.target = target
         let texture = SKTexture (imageNamed: "spr_cannon_blue")
         super.init(texture: texture,color:SKColor(),size:texture.size())
     }
@@ -24,9 +29,18 @@ class Bullet : SKSpriteNode {
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+    
     func updateDelta(delta: NSTimeInterval){
-     //   if self.box.intersects(self.target.box){
-     //       self.target.takeDamage(self.damage)
-    //    }
+        if position.x > target.position.x{
+            position.x -= 5
+        } else if position.x < target.position.x{
+            position.x += 5
+        }
+        
+        if position.y > target.position.y{
+            position.y -= 5
+        } else if position.y < target.position.y{
+            position.y += 5
+        }
     }
 }
